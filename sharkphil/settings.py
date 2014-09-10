f181463611e0892e5ceedff453257ded26cd69b1
ryan <ryan.wong022@gmail.com>
2014-09-10 13:49:39 +0800

    server settings database

--- sharkphil/settings.py
+++ sharkphil/settings.py
@@ -86,21 +86,21 @@ WSGI_APPLICATION = 'sharkphil.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-    DATABASES = {
-        'default': {
-            # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            # Or path to database file if using sqlite3.
-            'NAME': 'landing',
-            # The following settings are not used with sqlite3:
-            'USER': 'rbwong',
-            'PASSWORD': '09228845964',
-            # Empty for localhost through domain sockets or
-            # '127.0.0.1' for localhost through TCP.
-            'HOST': 'localhost',
-            'PORT': '',                      # Set to empty string for default.
-        }
+DATABASES = {
+    'default': {
+        # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # Or path to database file if using sqlite3.
+        'NAME': 'landing',
+        # The following settings are not used with sqlite3:
+        'USER': 'rbwong',
+        'PASSWORD': '09228845964',
+        # Empty for localhost through domain sockets or
+        # '127.0.0.1' for localhost through TCP.
+        'HOST': 'localhost',
+        'PORT': '',                      # Set to empty string for default.
     }
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
